969061de0dcabdb7182811facf3d1193982b6be2
정우철 <oocheol@naver.com>
2022-10-19 19:03:40 +0900

    React init

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>test</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
