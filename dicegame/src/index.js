a1380e8c0cb4c3a51f62e8d2ed896c82d23ba2ea
정우철 <oocheol@naver.com>
2022-10-19 19:35:35 +0900

    dice Game

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const product = '맥북';
+
+ReactDOM.render(
+        <h1>나만의 {product} 주문하기</h1>
+        ,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
